bcae1c5d1953bc67ba61bf53616e20c837593285
Gustavo Alves de Souza <gustavo-gustavo20@hotmail.com>
2020-06-23 23:12:57 -0300

    Added: Float add book button

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
+import Container from '@material-ui/core/Container';
+
 import Navbar from '../../components/Navbar'
 import Book from '../../components/Book'
+import FloatAddButton from '../../components/FloatAddButton'
 
 export default function Home() {
     return (
         <>
             <Navbar />
-            <Book
-                title="Clean Code"
-                author="Robert Cecil Martin"
-                description="Mesmo um código ruim pode funcionar. Mas se ele não for limpo, pode acabar com uma empresa de desenvolvimento."
-                image="https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L._SX374_BO1,204,203,200_.jpg"
-            />
+
+            <Container maxWidth="sm">
+                <Book
+                    title="Clean Code"
+                    author="Robert Cecil Martin"
+                    description="Mesmo um código ruim pode funcionar. Mas se ele não for limpo, pode acabar com uma empresa de desenvolvimento."
+                    image="https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L._SX374_BO1,204,203,200_.jpg"
+                />
+            </Container>
+
+            <FloatAddButton />
         </>
     );
 };
