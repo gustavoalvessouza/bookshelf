99bce76f62742f32e9b19f7b3739fd9e69a0ebe8
Gustavo Alves de Souza <gustavo-gustavo20@hotmail.com>
2020-06-23 22:51:46 -0300

    Added: Book component structure

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import Navbar from '../../components/Navbar'
+import Book from '../../components/Book'
 
 export default function Home() {
     return (
         <>
             <Navbar />
-            <h1>Bookshelf homepage</h1>
+            <Book
+                title="Clean Code"
+                author="Robert Cecil Martin"
+                description="Mesmo um código ruim pode funcionar. Mas se ele não for limpo, pode acabar com uma empresa de desenvolvimento."
+                image="https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L._SX374_BO1,204,203,200_.jpg"
+            />
         </>
     );
 };
